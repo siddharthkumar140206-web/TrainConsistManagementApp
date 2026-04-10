d9eeb75d5e411b119a5e226f81aad21562e67954
siddharthkumar140206-web <siddharthkumar140206@gmail.com>
2026-04-10 09:46:49 +0530

    sidharth added UC5-Preserve_insertion_Order_of_Bogies

--- App/src/TrainConsistManagementApp.java
+++ App/src/TrainConsistManagementApp.java
@@ -1,29 +1,26 @@
-import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
- class TrainConsistManagementUC3 {
+ class TrainConsistManagement {
     public static void main(String[] args) {
 
         System.out.println("==========================================");
-        System.out.println("UC3 - Track Unique Bogie IDs");
+        System.out.println("UC5 - Preserve Insertion Order of Bogies");
         System.out.println("==========================================\n");
 
-        Set<String> bogies = new HashSet<>();
+        Set<String> formation = new LinkedHashSet<>();
 
-        bogies.add("BG101");
-        bogies.add("BG102");
-        bogies.add("BG103");
-        bogies.add("BG104");
+        formation.add("Engine");
+        formation.add("Sleeper");
+        formation.add("Cargo");
+        formation.add("Guard");
 
-        bogies.add("BG101");
-        bogies.add("BG102");
+        formation.add("Sleeper");
 
-        System.out.println("Bogie IDs After Insertion:");
-        System.out.println(bogies);
+        System.out.println("Final Train Formation:");
+        System.out.println(formation);
 
-        System.out.println("\nNote:") ;
-        System.out.println("Duplicates are automatically ignored by HashSet.");
-
-        System.out.println("\nUC3 uniqueness validation completed...");
+        System.out.println("\nLinkedHashSet preserves insertion order and removes duplicates automatically.");
+        System.out.println("\nUC5 formation setup completed...");
     }
 }
